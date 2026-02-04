38a34170a31010fa0be2d2f0ed188d447968b30d
vatsalmsharma <vatsalmsharma@gmail.com>
2026-02-03 23:09:40 -0700

    Word Frequency Count

---
+++ src/word_count.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+spark = (
+            SparkSession
+            .builder
+            .getOrCreate()
+)
+
+spark.sparkContext.setLogLevel("WARN")
+
+results = (
+    spark.read.text("../data/gutenberg_books/1342-0.txt")
+    .select(F.split(F.col("value"), " ").alias("line"))
+    .select(F.explode(F.col("line")).alias("word"))
+    .select(F.lower(F.col("word")).alias("word"))
+    .select(F.regexp_extract(F.col("word"), "[a-z']*", 0).alias("word"))
+    .where(F.col("word") != "")
+    .groupby("word")
+    .count()
+)
+
+results.orderBy("count", ascending=False).show(10)
